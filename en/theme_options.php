56b6163e26869f02c0caac58cceb6f57eecaf810
Tim Bischoff <tim.bischoff@bisweb.de>
2025-07-31 14:52:25 +0200

    add theme setting translations

---
+++ en/theme_options.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright Tim Bischoff - Software Developer
+ * @link https://bisweb.de
+ * @author Tim Bischoff - Software Developer <tim.bischoff@bisweb.de>
+ */
+
+$sLangName = 'English';
+
+$aLang = array(
+    'charset' => 'UTF-8',
+
+    'SHOP_THEME_sGitHubUrl'        => 'GitHub Account-URL',
+    'HELP_SHOP_THEME_sGitHubUrl'   => 'E.g.: https://github.com/BisWebStore',
+);
